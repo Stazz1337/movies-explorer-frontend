64a3973ef804f3252eb7744e19e41f3f0b5422b8
Stas Tolmachev <stusik@yandex.ru>
2023-08-10 13:50:19 +0300

    0.1

--- src/old-components/ProtectedRoute.js
+++
@@ -1,7 +0,0 @@
-import { Navigate } from "react-router-dom";
-
-const ProtectedRouteElement = ({ isLoggedIn, element }) => {
-  return isLoggedIn ? element : <Navigate to="/sign-in" replace />;
-};
-
-export default ProtectedRouteElement;
